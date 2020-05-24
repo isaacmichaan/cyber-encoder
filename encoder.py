d64edaf721016dbd67d6cd1d0ae0b7514aa5a665
Isaac Michaan <46087877+isaacmichaan@users.noreply.github.com>
2020-05-25 02:43:35 +0300

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,17 @@
+import base64
+
+f = open('/var/www/html/client.py', 'r')
+client = f.read()
+f.close()
+
+#encoded == base64(client)
+encoded = base64.b64encode(str.encode(client)).decode('utf-8')
+
+d = int(len(encoded)/2)
+part1 = encoded[:d]
+part2 = encoded[d:]
+
+newpayload = part1 + '+' + part2
+
+with open("/var/www/html/encoded", "w") as f:
+	f.write(newpayload)
